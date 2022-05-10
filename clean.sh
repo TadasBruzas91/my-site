41ef83af76206d3bcf50193e537ca65b62a5cbc6
Tadas Bružas <tadasbruzas91@gmail.com>
2022-05-10 22:01:22 +0300

    Add docker-compose development files.

---
+++ clean.sh
@@ -0,0 +1,4 @@
+sudo rm -r front-end/node_modules && echo "node_modules removed"
+sudo rm -r front-end/certbot && echo "certbot removed"
+sudo rm -r front-end/nginx/logs && echo "logs removed"
+sudo rm -r front-end/.parcel-cache && echo "parcel-cache removed"
--- dev-start.sh
+++ dev-start.sh
@@ -3,3 +3,4 @@
 # Build front-end
 cd front-end && npm i && npm run build && cd ..
 
+docker-compose -f ./docker-compose.dev.yml up -d --build
