d6077f6c4c4a2841cc7abc4ff694729cc4350fe1
Tadas Bružas <tadasbruzas91@gmail.com>
2022-05-14 16:04:08 +0300

    Optimize dev script.

--- scripts/dev-restart.sh
+++ scripts/dev-restart.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+cd front-end && npm run build && cd ..
+cd react-dashboard && npm run build && cd ..
+
 docker-compose -f ./docker-compose.dev.yml restart
--- scripts/dev-start.sh
+++ scripts/dev-start.sh
@@ -2,5 +2,6 @@
 
 # Build front-end
 cd front-end && npm i && npm run build && cd ..
+cd react-dashboard && npm i && npm run build && cd ..
 
 docker-compose -f ./docker-compose.dev.yml up -d --build
