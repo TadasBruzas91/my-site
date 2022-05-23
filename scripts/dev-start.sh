0001a85f75f012cee4bccb1c91a153c3928c963c
Tadas Bružas <tadasbruzas91@gmail.com>
2022-05-23 22:01:54 +0300

    Update dev scripts.

--- scripts/dev-start.sh
+++ scripts/dev-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Build front-end
 cd front-end && npm i && npm run build && cd ..
-cd react-dashboard && npm i && npm run build && cd ..
+co back-end && npm i && cd ..
+cd react-dashboard && npm i && cd ..
 
 docker-compose -f ./docker-compose.dev.yml up -d --build
