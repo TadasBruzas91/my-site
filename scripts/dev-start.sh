e8c3afdce4d26da3604933244742afa6d6620191
Tadas Bružas <tadasbruzas91@gmail.com>
2022-05-23 22:12:46 +0300

    Fix typo in dev scripts.

--- scripts/dev-start.sh
+++ scripts/dev-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd front-end && npm i && npm run build && cd ..
-co back-end && npm i && cd ..
-cd react-dashboard && npm i && cd ..
+cd back-end && npm i && cd ..
+cd react-dashboard && npm i && npm run build && cd ..
 
 docker-compose -f ./docker-compose.dev.yml up -d --build
